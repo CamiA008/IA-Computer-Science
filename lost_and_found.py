5e7ad17d661e28f0464478f678b68ab7545dcb31
CamiA008 <camila.alier2008@icloud.com>
2026-09-16 08:17:19 -0600

    Update item addition form with select boxes
    Replaced text inputs with select boxes for category, color, and location found in the item addition form.

--- lost_and_found.py
+++ lost_and_found.py
@@ -163,14 +163,14 @@ def admin_page():
 
     if st.session_state.show_form == True:
 
-        with st.form("add_item"):
+         with st.form("add_item"):
             item_name = st.text_input("Item Name")
 
-            category = st.text_input("Category")
+            category = st.selectbox(["Clothing","Electronics", "Lunchbox", "Water Bottle", "Pencil Case"])
             brand = st.text_input("Brand")
-            color = st.text_input("Color")
+            color = st.selectbox(["Red","Orange", "Yellow", "Green", "Blue", "Purple"])
 
-            location_found = st.text_input("Location found")
+            location_found = st.selectbox(["Cafeteria","Central Plaza", "Buses", "Gym", "High School Building", "Middle School Building", "Elementary Building", "Preschool Building"])
             date_found = st.date_input("Date found")
             image = st.file_uploader("Image")
 
